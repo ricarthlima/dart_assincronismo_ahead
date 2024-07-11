a7f3ec3eb335f8b386116df2bf98cd67878f1679
Ricarth Lima <ricarth.lima@gmail.com>
2024-07-11 12:06:39 -0300

    aula03

--- bin/dart_assincronismo_ahead.dart
+++ bin/dart_assincronismo_ahead.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
 
+import 'package:dart_assincronismo_ahead/api_key.dart';
 import 'package:http/http.dart';
 
 void main() {
   //requestData();
-  requestDataAsync();
+  //requestDataAsync();
+  sendDataAsync({
+    "id": "IDNEW",
+    "name": "Sebastian",
+    "lastName": "Montanha",
+    "accountType": "Prata",
+    "balance": 333.0,
+  });
 }
 
 requestData() {
@@ -24,11 +32,42 @@ requestData() {
   print("Deveria acontecer depois, mas acontece antes.");
 }
 
-requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/ricarthlima/413c0aefe6c6abc464581c29029c8ace/raw/d6c2f9da4bf96ac36f4fa83fb001156f8ace265d/accounts.json";
 
   Response response = await get(Uri.parse(url));
   print(json.decode(response.body)[0]);
   print("Deveria acontecer depois, e acontece!");
+
+  return json.decode(response.body);
+}
+
+sendDataAsync(Map<String, dynamic> toSend) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(toSend);
+  String content = json.encode(listAccounts);
+
+  // Não funciona
+  // String url =
+  //     "https://gist.githubusercontent.com/ricarthlima/413c0aefe6c6abc464581c29029c8ace/raw/d6c2f9da4bf96ac36f4fa83fb001156f8ace265d/accounts.json";
+
+  // Response response = await post(Uri.parse(url), body: content);
+
+  String url = "https://api.github.com/gists/413c0aefe6c6abc464581c29029c8ace";
+  Response response = await post(
+    Uri.parse(url),
+    headers: {
+      "Authorization": "Bearer $githubApiKey",
+    },
+    body: json.encode({
+      "description": "accounts.json",
+      "public": true,
+      "files": {
+        "accounts.json": {"content": content}
+      }
+    }),
+  );
+
+  print(response.statusCode);
 }
--- test/dart_assincronismo_ahead_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_assincronismo_ahead/dart_assincronismo_ahead.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
